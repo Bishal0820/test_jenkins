f383b603f4a9fa14a50dc40694ab41659c77a2ab
Bishal Paudel <73044633+Bishal0820@users.noreply.github.com>
2022-06-29 21:27:48 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,12 +2,14 @@ terraform {
   backend "s3" {
     bucket         = "bishal-is-a-joke"
     key            = "state/"
+    profile = "default"
     region         = "us-east-1"
   }
 
 }
 provider "aws" {
   region     = "us-east-1"
+  profile = "default"
   access_key = "AccessKeyId"
   secret_key = "SecretKeyId"
 }
